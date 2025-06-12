87669d911f9fd2dfb83538b1b62c4424bcf5f6ea
Joyce Kelly <joyce.kss.06@gmail.com>
2025-06-12 08:34:42 -0300

    Registrando os modelos na página do admin.

--- back/app/admin.py
+++ back/app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Usuario, Ambiente, Sensor, Historico
+from django.contrib.auth.admin import UserAdmin
+
+# Registro dos modelos
+admin.site.register(Usuario)  
+admin.site.register(Ambiente)  
+admin.site.register(Sensor)  
+admin.site.register(Historico)
